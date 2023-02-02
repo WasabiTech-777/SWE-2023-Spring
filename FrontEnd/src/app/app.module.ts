acb0f8a87e9e3f14ca9c06c8762047d4d09bca6c
Saviely Barreto <saviely1230@gmail.com>
2023-02-01 21:18:47 -0500

    added Demo component and tool bar feature

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
 
 import { AppComponent } from './app.component';
+import { DemoPageComponent } from './demo-page/demo-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoPageComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
