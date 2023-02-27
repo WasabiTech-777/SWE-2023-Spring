34f8411988c34082eb9bd12b276ffc35890d02e8
Kohki Taniguchi <kohki3325@outlook.com>
2023-02-27 18:11:24 -0500

    fixed duplicate modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
