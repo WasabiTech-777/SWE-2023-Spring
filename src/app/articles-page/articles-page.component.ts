caa43b50d9c2a0e4551ecbd66f58517549300c7d
Kohki Taniguchi <kohki3325@outlook.com>
2023-04-19 19:58:02 -0400

    updated articles page

--- src/app/articles-page/articles-page.component.ts
+++ src/app/articles-page/articles-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-articles-page',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./articles-page.component.css']
 })
 export class ArticlesPageComponent {
-
+  constructor(private http: HttpClient) { }
+  dataSource = ArticleList
+  displayedColumns: string[] = ['title', 'url', 'length'];
+  ngOnInit() {
+    //this.http.get(`${environment.serverUrl}/article/0`).subscribe(response => console.log(response));
+  }
 }
+export interface Article {
+  title: string;
+  url: string;
+  length: number;
+}
+const ArticleList: Article[] = [
+  {title: "Japan", url: "https://simple.wikipedia.org/wiki/Japan", length: 525}
+];
